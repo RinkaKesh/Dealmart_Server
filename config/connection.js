41fc50dbfe374fc9795dfa53a5220c1a8ce8d6e4
RinkaKesh <keshrinka2013@gmail.com>
2024-06-25 12:06:24 +0530

    added

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,16 @@
 const mongoose=require("mongoose")
 require("dotenv").config()
 
-const connection=mongoose.connect(process.env.mongoURI)
-
+const connection=mongoose.connect(process.env.mongoURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+mongoose.connection.on("connected", () => {
+    console.log("Connected to MongoDB");
+});
+mongoose.connection.on("error", (err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
 module.exports={connection}
+
+
